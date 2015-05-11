92ab4d5eaf6f8922a7b5931bc3ac423523e4fbd2
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-05-11 14:55:48 -0700

    exercise-19

---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Cynthia Whitlatch on 5/11/15.
+//  Copyright (c) 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
